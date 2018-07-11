7c81d018539cb9240d13b75c4e23fe55dce3b962
hzw517262544 <517262544@qq.com>
2018-07-11 23:11:12 +0800

    20180711

---
+++ new-star-blog/src/main/java/com/hao/blog/service/VoteServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hao.blog.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hao.blog.domain.Vote;
+import com.hao.blog.repository.VoteRepository;
+
+/**
+ * Vote 服务.
+ * 
+ * @since 1.0.0 2017年4月9日
+ * @author <a href="https://waylau.com">Way Lau</a>
+ */
+@Service
+public class VoteServiceImpl implements VoteService {
+
+	@Autowired
+	private VoteRepository voteRepository;
+	/* (non-Javadoc)
+	 * @see VoteService#removeVote(java.lang.Long)
+	 */
+	@Override
+	@Transactional
+	public void removeVote(Long id) {
+		voteRepository.delete(id);
+	}
+	@Override
+	public Vote getVoteById(Long id) {
+		return voteRepository.findOne(id);
+	}
+
+}
